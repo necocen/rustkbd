c6f53bdd46b542aafbd29f44a9ac038a528e810e
necocen <necocen@gmail.com>
2022-05-29 13:37:51 +0900

    add delay

--- necoboard-petit-ec/src/key_matrix.rs
+++ necoboard-petit-ec/src/key_matrix.rs
@@ -122,7 +122,7 @@ impl<
                 delay.delay_us(5);
 
                 let val: u16 = adc.read(&mut *adc_pin).unwrap_or(0);
-                delay.delay_us(5);
+                delay.delay_us(10);
                 // TODO: 何らかのフィルタ
                 if val > 30 {
                     let key_identifier = if self.is_left {
